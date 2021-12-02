fa833b11417b6abba5cd3844e75af3046bb433c8
fproko <fernandoprokopiuk@gmail.com>
2021-12-02 09:08:18 -0300

    Resolución parte comunicación serial.

--- TP2/SerialService/SerialManager.h
+++ TP2/SerialService/SerialManager.h
@@ -1,9 +1,32 @@
 #ifndef __SERIALMANAGER_H__
 #define __SERIALMANAGER_H__
 
+/**
+ * @brief Función para abrir puerto serie en la PC.
+ * 
+ * @param pn Número de puerto.
+ * @param baudrate Baudrate deseado.
+ * @return int File Descriptor.
+ */
 int serial_open(int pn,int baudrate);
+
+/**
+ * @brief Función para mandar datos por puerto serie de la PC.
+ * 
+ * @param pData Ptr a buffer a enviar.
+ * @param size Tamaño del dato a enviar.
+ */
 void serial_send(char* pData,int size);
 void serial_close(void);
+
+/**
+ * @brief Función recibir datos por puerto serie de la PC. No es bloqueante.
+ * 
+ * @param buf Ptr a buffer donde se recibe.
+ * @param size Tamaño del buffer.
+ * @return int <0> Si no hay datos.
+ *             <1> Si hay datos en buffer.
+ */
 int serial_receive(char* buf,int size);
 
 #endif /* __SERIALMANAGER_H__ */
--- TP2/SerialService/main.c
+++ TP2/SerialService/main.c
@@ -6,15 +6,99 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+
 #include "SerialManager.h"
 
+#define PORT_CIAA 1
+#define BAUDRATE 115200
+#define BUFFER_SIZE 100
+#define CMP_SIZE 5
+#define ONE_DATA 1
+#define CMP_MATCH 0
 
+/**
+ * @brief Handler de serial thread.
+ * 
+ * @param message 
+ * @return void* 
+ */
+void *tcp_thread(void *message)
+{
+	printf("%s\n", (const char *)message);	   //Imprime por consola el parámetro que recibio, previamente lo castea.
+	
+	while (1)
+	{
+		usleep(10000); //10 ms
+	}
+	
+
+	return NULL;
+}
 
 int main(void)
 {
-	printf("Inicio Serial Service\r\n");
+	int result;
+	char inputBuffer[BUFFER_SIZE];
+	char outputBuffer[BUFFER_SIZE];
+	char key;
+	char msj_hardcode[] = ">OUTS:2,2,2,2\r\n"; //FIXME: Borrar luego, test respuesta EDU-CIAA
 	
-	
+	pthread_t tcp; //Se declara handler de hilos.
+	const char *message1 = "Inicio TCP Service";
+
+	if (pthread_create(&tcp, NULL, tcp_thread, (void *)message1) != 0)
+	{
+		perror("Error al crear tcp thread");
+	}
+
+	//Se abre comunicación serial
+	if (result = serial_open(PORT_CIAA, BAUDRATE) == 1)
+	{
+		printf("Error al abrir comunicación serial\n");
+		exit(1);
+	}
+
+	serial_send(msj_hardcode, sizeof(msj_hardcode)); //FIXME: Borrar luego, test respuesta EDU-CIAA
+
+	while (1)
+	{
+		if (serial_receive(inputBuffer, sizeof(inputBuffer)) > 0)
+		{
+			printf("Se recibió:%s", inputBuffer);
+			if (sscanf(inputBuffer, ">TOGGLE STATE:%c\r\n", &key) == ONE_DATA)
+			{
+				//Ingresa si el número de entradas coinciden y se asignaron correctamente.
+				if (key >= '0' && key <= '3')
+				{
+					snprintf(outputBuffer, sizeof(outputBuffer), ":LINE%cTG\n", key);
+					printf("Se envió:%s", outputBuffer);
+
+					//TODO: enviar por socket.
+					//write(fd,outputBuffer,strlen(outputBuffer)+1);
+				}
+			}
+			else if (strncmp(inputBuffer, ">OK\r\n", CMP_SIZE) == CMP_MATCH)
+			{
+				printf("ok\n");
+			}
+			else
+			{
+				printf("Error en trama recibida\n");
+			}
+		}
+		usleep(10000); //10 ms
+	}
+
+	printf("Se espera por finalización de tcp thread\n");
+	if (pthread_join(tcp, NULL) != 0) //Hilo principal se queda acá bloqueado.
+	{
+		perror("Error en tcp thread");
+	}
+
 	exit(EXIT_SUCCESS);
 	return 0;
 }
